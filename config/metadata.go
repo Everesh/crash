d4c288fc65c6393540a36b54d0308d1546afb4be
Everesh <everesh@proton.me>
2026-05-24 20:13:55 +0200

    update default PS1

--- config/metadata.go
+++ config/metadata.go
@@ -5,4 +5,4 @@ import "fmt"
 const AppName = "crash"
 const Version = "0.0.0"
 
-var PS1 = fmt.Sprintf("%s:%s$ ", AppName, Version)
+var PS1 = fmt.Sprintf("\x1b[31;1m%s\x1b[0m:\x1b[33m%s\x1b[0m$ ", AppName, Version)
